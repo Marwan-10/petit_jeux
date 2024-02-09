dd2ac420c610c3280464234020de8c366291a15d
Marwan-10 <151082477+Marwan-10@users.noreply.github.com>
2024-02-09 11:31:13 +0100

    Update generateurMDP.py

--- generateurMDP/generateurMDP.py
+++ generateurMDP/generateurMDP.py
@@ -1,4 +1,3 @@
-#Password Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -9,8 +8,6 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 mdp=""
 for i in range(0,nr_letters):
@@ -23,8 +20,6 @@ for i in range(0,nr_numbers):
   mdp+=random.choice(numbers)
   
 print(mdp)
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 mdplist=[]
 for i in range(0,nr_letters):
   mdplist+=random.choice(letters)
